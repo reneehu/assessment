1d9c3a973f8c327bb126d858ffa733e6a1d55d21
reneehu <reneehu217@gmail.com>
2017-09-20 18:54:27 -0400

    ah have to  remove duplicate string

--- app/strings.js
+++ app/strings.js
@@ -2,7 +2,9 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+  	var res = str.split('').splice(0, (str.length - amount)).join('');
+  	console.log(str, amount,res);
+  	return res;
   },
 
   wordWrap: function(str, cols) {
