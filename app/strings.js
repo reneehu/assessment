968ecef381ce172631c3fc3c858515065a55480c
reneehu <reneehu217@gmail.com>
2017-09-23 16:52:07 -0400

    update

--- app/strings.js
+++ app/strings.js
@@ -2,9 +2,16 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-  	var res = str.split('').splice(0, (str.length - amount)).join('');
-  	console.log(str, amount,res);
-  	return res;
+  	// var res = str.split('').splice(0, (str.length - amount)).join('');
+  	// console.log(str, amount,res);
+  	// return res;
+   	var arr = str.split('');
+   	var resArr = [];
+   	for(i=0; i<arr.length; i++) {
+   		if(arr[i]===arr[i+2]) {
+   			
+   		}
+   	}
   },
 
   wordWrap: function(str, cols) {
